341008aa1d47fabe11dcc91b3b24d68dbc0195ff
Kasia <Kasia@DESKTOP-FTJI9GJ>
2018-02-08 16:18:55 +0100

    Dodanie pierwszej strony

--- MapApplication/Models/AtractionDb.cs
+++ MapApplication/Models/AtractionDb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,16 +13,21 @@ namespace MapApplication.Models
     {
         [Key]
         public int ID { get; set; }
-
+        [DisplayName("Nazwa atrakcji")]
         [StringLength(50)]
         public string AtractionName { get; set; }
+        [DisplayName("Szerokość geograficzna")]
         public double Latitude { get; set; }
+        [DisplayName("Długość geograficzna")]
         public double Longitude { get; set; }
 
         [StringLength(50)]
+        [DisplayName("Nazwa miasta")]
         public string City { get; set; }
         [StringLength(150)]
+        [DisplayName("Opis atrakcji")]
         public string Decription { get; set; }
+        [DisplayName("Zdjęcie atrakcji")]
         public string MyImagesFile { get; set; }
         public virtual City CityData { get; set; }
     }
--- MapApplication/Models/City.cs
+++ MapApplication/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace MapApplication.Models
     public class City
     {
         public int Id { get; set; }
+        [DisplayName("Miasto")]
         public string Name { get; set; }
+        [DisplayName("Szerokość geograficzna miasta")]
         public double CityLatitude { get; set; }
+        [DisplayName("Długość geograficzna miasta")]
         public double CityLongitude { get; set; }
         public virtual ICollection <AtractionDb> AtractionDb { get; set; }
     }
